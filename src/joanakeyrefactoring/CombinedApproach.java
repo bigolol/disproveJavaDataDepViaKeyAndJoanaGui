b44c116872ffa96e70430e9650edbeabdbaf52c5
Michael Kirsten <kirsten@ira.uka.de>
2017-09-12 02:37:04 +0200

    Removed unused import

--- src/joanakeyrefactoring/CombinedApproach.java
+++ src/joanakeyrefactoring/CombinedApproach.java
@@ -20,7 +20,6 @@ import edu.kit.joana.util.Stubs;
 import edu.kit.joana.wala.core.SDGBuilder.ExceptionAnalysis;
 import edu.kit.joana.wala.core.SDGBuilder.FieldPropagation;
 import edu.kit.joana.wala.core.SDGBuilder.PointsToPrecision;
-import joanakeygui.AddSourceDialogController;
 
 import java.io.BufferedReader;
 import java.io.File;
